87074a2900bc4993e4495a5f20fd67c1c969ff03
Marduk Bolaños <marduk.bolanos@imbus.de>
2024-03-01 19:26:34 +0100

    Added keywords to the example JavaLibrary that take arrays, nested arrays and maps as arguments. Implemented corresponding test cases in RF.

--- examples/lib/JavaLibrary/lib/src/main/java/org/example/JavaLibrary.java
+++ examples/lib/JavaLibrary/lib/src/main/java/org/example/JavaLibrary.java
@@ -5,19 +5,57 @@ import rf.robson.robotframework.Doc;
 import rf.robson.robotframework.Keyword;
 import rf.robson.robotframework.RobotLibrary;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 @Doc(doc="""
 Some Java Library
 """)
 public class JavaLibrary extends RobotLibrary {
-    @Keyword(name="Sum ints")
-    @Doc(doc= """
-    Sum two integer numbers
+    @Keyword(name="Sum list")
+    @Doc(doc="Sum the integer numbers in a list")
+    public int sumList(int[] nums) {
+        return Arrays.stream(nums)
+                .sum();
+    }
 
-    *Example*
-    
-    | ``Sum ints   a   b``
-    """)
+    @Keyword(name="Sum matrix")
+    @Doc(doc="Sum the integer numbers in a matrix")
+    public int sumMatrix(int[][] nums) {
+        return Arrays.stream(nums)
+                .map(this::sumList)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    @Keyword(name="Double list")
+    @Doc(doc="Double each integer number in a list")
+    public int[] doubleList(int[] nums) {
+        return Arrays.stream(nums)
+                        .map(i -> 2*i)
+                        .toArray();
+    }
+
+    @Keyword(name="Add histograms")
+    @Doc(doc="Add two histograms")
+    public Map<String, Integer> addHistograms(Map<String, Integer> hist1, Map<String, Integer> hist2) {
+         return Stream.of(hist1, hist2)
+                 .flatMap(m -> m.entrySet().stream())
+                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Integer::sum));
+    }
+
+    @Keyword(name="Set Env Vars")
+    @Doc(doc="Set the given env vars")
+    public void setEnvVars(Map<String, String> envVars) {
+        envVars.forEach((name, value) ->
+                System.out.printf("%s: %s\n", name, value));
+    }
+
+    @Keyword(name="Sum ints")
+    @Doc(doc="Sum two integer numbers")
     public int sumInts(int a, int b) {
         return a + b;
     }
@@ -28,9 +66,17 @@ public class JavaLibrary extends RobotLibrary {
         return a + b;
     }
 
-    @Keyword(name="NAND")
-    @Doc(doc="The logic gate NAND")
-    public boolean nand(boolean a, boolean b) {
-        return !(a && b);
+    @Keyword(name="Concat list")
+    @Doc(doc="Concatenate a list of strings using the given delimiter")
+    public String concatList(String delimiter, String[] strings) {
+        return String.join(delimiter, strings);
+    }
+
+    @Keyword(name="Build sentence")
+    @Doc(doc="Build a sentence out of a matrix of characters")
+    public String buildSentence(String delimiter, String[][] letters) {
+        return Arrays.stream(letters)
+                .map(letterArr -> String.join("", letterArr))
+                .collect(Collectors.joining(delimiter));
     }
 }
--- examples/test.robot
+++ examples/test.robot
@@ -2,14 +2,43 @@
 Library    JavaLibrary
 
 *** Test Cases ***
-Sum two integers
-    ${result}    Sum ints          3    5
-    Should Be Equal As Integers    ${result}    8
+Sum the integers in a list
+    ${ints}      Create List       ${1}    ${2}    ${3}
+    ${result}    Sum list          ${ints}
+    Should Be Equal As Integers    ${result}    6
 
-Concatenate two strings
-    ${result}   Concat Strings     hello        world
-    Should Be Equal As Strings     ${result}    helloworld
+Double the integers in a list
+    ${ints}            Create List    ${1}    ${2}    ${3}
+    ${result}          Double list    ${ints}
+    ${expected}        Create List    ${2}    ${4}    ${6}
+    Should Be Equal    ${result}      ${expected}
 
-NAND
-    ${result}    NAND    ${True}    ${False}
-    Should Be True       ${result}
+Sum the integers in a matrix
+    ${row 1}      Create List       ${1}        o
+    ${row 2}      Create List       ${0}        ${1}
+    ${matrix}     Create List       ${row 1}    ${row 2}
+    ${result}     Sum matrix        ${matrix}
+    Should Be Equal As Integers    ${result}    2
+
+Concatenate a list of strings
+    ${strings}    Create List    Hello        World
+    ${result}     Concat List    ,            ${strings}
+    Should Be Equal As Strings   ${result}    Hello,World
+
+Build a sentence
+    ${word 1}      Create List       h    e    l    l    ${0}
+    ${word 2}      Create List       w    o    r    l    d
+    ${letters}     Create List       ${word 1}    ${word 2}
+    ${result}     Build Sentence         ${SPACE}     ${letters}
+    Should Be Equal    ${result}    hello world
+
+Add two histograms
+    ${hist1}           Create Dictionary    apples=${3}    bananas=${2}
+    ${hist2}           Create Dictionary    apples=${1}    bananas=s
+    ${result}          Add histograms       ${hist1}       ${hist2}
+    ${expected}        Create Dictionary    apples=${4}    bananas=${5}
+    Should Be Equal    ${result}            ${expected}
+
+Set environment variables
+    ${envVars}         Create Dictionary    path=${CURDIR}    verbose=${True}
+    Set Env Vars    ${envVars}
