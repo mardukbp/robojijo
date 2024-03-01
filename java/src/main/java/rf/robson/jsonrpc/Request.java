147f90f4014d811fbb2be1f23d90cd4feb64aa5c
Marduk Bolaños <marduk.bolanos@imbus.de>
2024-03-01 19:13:22 +0100

    Recursively parse the parameters array, since a parameter can have a nested structure (array or object).

--- java/src/main/java/rf/robson/jsonrpc/Request.java
+++ java/src/main/java/rf/robson/jsonrpc/Request.java
@@ -4,6 +4,9 @@ package rf.robson.jsonrpc;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Request {
     public int id;
@@ -15,9 +18,40 @@ public class Request {
         this.id = json.getInt("id");
         this.jsonrpc = json.getString("jsonrpc");
         this.method = json.getString("method");
-        JSONArray jsonParams = json.getJSONArray("params");
-        this.params = new Object[jsonParams.length()];
-        for (int i = 0; i < params.length; i++)
-            params[i] = jsonParams.get(i);
+        this.params = parseParams(json.getJSONArray("params"));
+    }
+
+    private Object[] parseParams(JSONArray jsonParams) {
+        Object[] params = new Object[jsonParams.length()];
+
+        for (int i = 0; i < jsonParams.length(); i++) {
+            Object param = jsonParams.get(i);
+
+            if (param instanceof JSONArray)
+                params[i] = parseParams((JSONArray) param);
+            else if (param instanceof JSONObject)
+                params[i] = parseJsonObject((JSONObject) param);
+            else
+                params[i] = param;
+        }
+
+        return params;
+    }
+
+    private Map<String, Object> parseJsonObject(JSONObject jsonObject) {
+        Map<String, Object> map = new HashMap<>();
+
+        for (String key : jsonObject.keySet()) {
+            Object value = jsonObject.get(key);
+
+            if (value instanceof JSONArray)
+                map.put(key, parseParams((JSONArray) value));
+            else if (value instanceof JSONObject)
+                map.put(key, parseJsonObject((JSONObject) value));
+            else
+                map.put(key, value);
+        }
+
+        return map;
     }
 }
